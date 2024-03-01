d950a03d1b1200e2d14ff2313375e4bdb0702574
Maruf OVi <imgoinin.iamovi@gmail.com>
2024-03-01 23:06:51 +0600

    add project

---
+++ README.md
@@ -0,0 +1,67 @@
+# PrintCLI
+
+PrintCLI is a command-line tool that allows users to quickly print code snippets in various programming languages. It supports a wide range of popular languages, making it a handy utility for developers who want to view or copy code snippets on the command line.
+
+## Installation
+
+To install PrintCLI globally, use the following command:
+
+```bash
+npm install -g printcli
+```
+
+## Usage
+
+After installation, you can use PrintCLI by typing printcli followed by the programming language you want to print. For example:
+
+```bash
+printcli python
+```
+
+This will display the Python code snippet for a "Hello World!" program - 
+
+```python
+print('Hello World!')
+```
+
+## Supported Languages
+
+PrintCLI currently supports the following programming languages:
+
+- Python
+- JavaScript
+- Java
+- C
+- C++
+- Ruby
+- Swift
+- Go
+- PHP
+- HTML
+- Brainfuck
+- Python3
+- TypeScript
+- Rust
+- Kotlin
+- Scala
+- Groovy
+- Bash
+- Perl
+- Lua
+- C#
+- Objective-C
+- Swift3
+- R
+- MATLAB
+- Ruby(again)
+- Perl6
+
+Feel free to check the `hello.js` file in the project for an up-to-date list of supported languages.
+
+## License
+
+[MIT](LICENSE)
+
+## Author
+
+[Maruf OVi](https://oviportfo.netlify.app/)
---
+++ hello.js
@@ -0,0 +1,31 @@
+// hello.js
+
+module.exports = {
+    python: "print('Hello World!')",
+    javascript: "console.log('Hello World!');",
+    java: "public class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println('Hello World!');\n\t}\n}",
+    c: "#include <stdio.h>\n\nint main() {\n\tprintf('Hello World!\\n');\n\treturn 0;\n}",
+    cpp: "#include <iostream>\n\nint main() {\n\tstd::cout << 'Hello World!' << std::endl;\n\treturn 0;\n}",
+    ruby: "puts 'Hello World!'",
+    swift: "print('Hello World!')",
+    go: "package main\n\nimport 'fmt'\n\nfunc main() {\n\tfmt.Println('Hello World!')\n}",
+    php: "<?php\n\techo 'Hello World!';\n?>",
+    html: "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>Hello World</title>\n\t</head>\n\t<body>\n\t\t<h2>haha</h2>\n\t</body>\n</html>",
+    brainfuck: "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.",
+    typescript: "console.log('Hello World!');",
+    rust: "fn main() {\n\tprintln!('Hello, World!');\n}",
+    kotlin: "fun main() {\n\tprintln('Hello World!')\n}",
+    scala: "object HelloWorld {\n\tdef main(args: Array[String]): Unit = {\n\t\tprintln('Hello World!')\n\t}\n}",
+    groovy: "println 'Hello World!'",
+    bash: "echo 'Hello World!'",
+    perl: "print 'Hello World!';",
+    lua: "print('Hello World!')",
+    csharp: "using System;\n\nclass HelloWorld {\n\tstatic void Main() {\n\t\tConsole.WriteLine('Hello World!');\n\t}\n}",
+    objectivec: "#import <Foundation/Foundation.h>\n\nint main() {\n\t@autoreleasepool {\n\t\tNSLog(@'Hello, World!');\n\t}\n\treturn 0;\n}",
+    swift3: "print('Hello World!')",
+    r: "cat('Hello World!\\n')",
+    matlab: "disp('Hello World!');",
+    ruby: "puts 'Hello World!'",
+    perl6: "say 'Hello World!';"
+  };
+  
